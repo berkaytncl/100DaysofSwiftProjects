671fac067288f0ba68d7e2f878a4401b99d1da39
Berkay Tuncel <berkaytncl98@gmail.com>
2023-08-05 00:11:37 +0300

    Build detail screen

---
+++ Project01/Project01/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  Project01
+//
+//  Created by Berkay Tuncel on 4.08.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
