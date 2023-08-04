6e0f39adc2cbf5b244d4593161083be00d8a9e2d
Berkay Tuncel <berkaytncl98@gmail.com>
2023-08-04 20:17:30 +0300

    Designed interface

--- Project01/Project01/ViewController.swift
+++ Project01/Project01/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class ViewController: UITableViewController {
     
     private var pictures = [String]()
     
@@ -24,5 +24,14 @@ final class ViewController: UIViewController {
             }
         }
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 }
-
