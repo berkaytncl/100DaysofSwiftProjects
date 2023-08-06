bf8f6c8131038e7f1b759ce4a86a62fbbdcdb2e3
Berkay Tuncel <berkaytncl98@gmail.com>
2023-08-06 12:55:22 +0300

    Improve game logic

--- Project02/Project02/ViewController.swift
+++ Project02/Project02/ViewController.swift
@@ -9,13 +9,15 @@ import UIKit
 
 final class ViewController: UIViewController {
 
-    @IBOutlet var button1: UIButton!
-    @IBOutlet var button2: UIButton!
-    @IBOutlet var button3: UIButton!
+    @IBOutlet private var button1: UIButton!
+    @IBOutlet private var button2: UIButton!
+    @IBOutlet private var button3: UIButton!
+    @IBOutlet private var scoreLabel: UILabel!
     
     private var countries = [String]()
     private var score = 0
     private var correctAnswer = 0
+    private var askedQuestions = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +36,11 @@ final class ViewController: UIViewController {
     }
     
     func askQuestion(action: UIAlertAction! = nil) {
+        guard askedQuestions < 10 else {
+            gameOver()
+            return
+        }
+        
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         
@@ -42,24 +49,51 @@ final class ViewController: UIViewController {
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
         title = countries[correctAnswer].uppercased()
+        
+        askedQuestions += 1
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         var title: String
+        var message: String
         
         if sender.tag == correctAnswer {
             title = "Correct"
             score += 1
+            message = "Your score is \(score)"
         } else {
             title = "Wrong"
             score -= 1
+            message = """
+            That’s the flag of \(countries[sender.tag].uppercased()),
+            Your score is \(score)
+            """
         }
         
-        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
         ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
         
         present(ac, animated: true)
+        
+        scoreLabel.text = "Score: \(score)"
+    }
+    
+    private func gameOver() {
+        let ac = UIAlertController(title: "Game Over", message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Play Again", style: .default, handler: playAgain))
+        
+        present(ac, animated: true)
+    }
+    
+    private func playAgain(action: UIAlertAction! = nil) {
+        score = 0
+        askedQuestions = 0
+        
+        scoreLabel.text = "Score: \(score)"
+        
+        askQuestion()
     }
 }
 
