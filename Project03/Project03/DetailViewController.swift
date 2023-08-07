2cefc048b1859371b3c3353852cfe97e45d3ae43
Berkay Tuncel <berkaytncl98@gmail.com>
2023-08-07 10:52:02 +0300

    Copy Project01 files for Project03

---
+++ Project03/Project03/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  Project03
+//
+//  Created by Berkay Tuncel on 7.08.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    var imagePosition: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = imagePosition
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let image = selectedImage {
+            imageView.image = UIImage(named: image)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+}
