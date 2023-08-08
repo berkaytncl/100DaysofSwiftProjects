00b7b4b1c270178c262094027d371141e4843e72
Berkay Tuncel <berkaytncl98@gmail.com>
2023-08-08 16:22:25 +0300

    Add simple browser with WKWebView

--- Project04/Project04/ViewController.swift
+++ Project04/Project04/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    private var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
-
-
 }
 
+extension ViewController: WKNavigationDelegate {
+    
+}
