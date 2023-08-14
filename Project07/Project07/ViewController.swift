0c94aa762c63ba5738428261711320c9852cd44a
Berkay Tuncel <berkaytncl98@gmail.com>
2023-08-14 15:41:33 +0300

    Add UITabBarItem

--- Project07/Project07/ViewController.swift
+++ Project07/Project07/ViewController.swift
@@ -14,12 +14,18 @@ final class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
         if let url = URL(string: urlString) {
             URLSession.shared.dataTask(with: url) { data, response, error in
                 if let error = error {
-                    print("Error: \(error)")
+                    self.showError()
                     return
                 }
                 
@@ -32,7 +38,11 @@ final class ViewController: UITableViewController {
             }.resume()
         }
     }
-    
+    private func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
     private func parse(json: Data) {
         let decoder = JSONDecoder()
         
