e74371783ff8222153b36c7739377239ddbbdf58
Berkay Tuncel <berkaytncl98@gmail.com>
2023-08-14 20:41:42 +0300

    Project08 Initial Commit

---
+++ Project08/Project08/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project08
+//
+//  Created by Berkay Tuncel on 14.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
