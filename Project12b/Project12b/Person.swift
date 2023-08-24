c16651259194345b552d06ed449c8444b0eedf6b
Berkay Tuncel <berkaytncl98@gmail.com>
2023-08-24 13:27:34 +0300

    Project12 Initial Commit

---
+++ Project12b/Project12b/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project12b
+//
+//  Created by Berkay Tuncel on 21.08.2023.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
