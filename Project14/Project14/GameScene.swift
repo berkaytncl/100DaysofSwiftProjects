89851d3392f0ae92850471182bb2d2f422169e56
Berkay Tuncel <berkaytncl98@gmail.com>
2023-09-03 13:37:22 +0300

    Add enemies, add show and hide methods of enemies

--- Project14/Project14/GameScene.swift
+++ Project14/Project14/GameScene.swift
@@ -11,6 +11,8 @@ class GameScene: SKScene {
     private var slots = [WhackSlot]()
     private var gameScore: SKLabelNode!
     
+    private var popupTime = 0.85
+    
     private var score = 0 {
         didSet {
             gameScore.text = "Score: \(score)"
@@ -35,10 +37,14 @@ class GameScene: SKScene {
         for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320)) }
         for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230)) }
         for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140)) }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.createEnemy()
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        createSlot(at: position)
+        
     }
     
     private func createSlot(at position: CGPoint) {
@@ -46,5 +52,25 @@ class GameScene: SKScene {
         slot.configure(at: position)
         addChild(slot)
         slots.append(slot)
+    }
+    
+    func createEnemy() {
+        popupTime *= 0.991
+        
+        slots.shuffle()
+        slots[0].show(hideTime: popupTime)
+        
+        if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 8 { slots[2].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 12 { slots[4].show(hideTime: popupTime) }
+        
+        let minDelay = popupTime / 2.0
+        let maxDelay = popupTime * 2
+        let delay = Double.random(in: minDelay...maxDelay)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+            self?.createEnemy()
+        }
     }
 }
