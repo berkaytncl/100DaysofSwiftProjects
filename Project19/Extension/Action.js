22e3497d548eb82e7f9baffd24bf643bc7be2bdd
Berkay Tuncel <berkaytncl98@gmail.com>
2023-09-20 18:46:51 +0300

    Communication established

---
+++ Project19/Extension/Action.js
@@ -0,0 +1,15 @@
+var Action = function() { }
+
+Action.prototype = {
+    
+run: function(paramaters) {
+    paramaters.completionFunction({"URL": document.URL, "title": document.title})
+},
+    
+finalize: function(paramaters) {
+    
+}
+    
+};
+    
+var ExtensionPreprocessingJS = new Action
