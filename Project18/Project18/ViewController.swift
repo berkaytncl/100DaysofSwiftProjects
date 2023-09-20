3526d8616eaf9c98292572a9e6110b3fdf9eae17
Berkay Tuncel <berkaytncl98@gmail.com>
2023-09-20 10:31:25 +0300

    Debugging

--- Project18/Project18/ViewController.swift
+++ Project18/Project18/ViewController.swift
@@ -11,7 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+//        print(1, 2, 3, 4, 5, separator: "-") //1-2-3-4-5
+//        print("Some message", "ss", terminator: "2") //Some message ss2
+//
+//        assert(1 == 1, "Math failure!")
+//        assert(1 == 2, "Math failure!")
+        
+//        for i in 1...100 {
+//            print("Got number \()")
+//        }
     }
 
 
