bd73b1b17c3e712f566dce998d8cd3839aa50c8c
Berkay Tuncel <berkaytncl98@gmail.com>
2023-09-20 12:11:52 +0300

    Project19 Initial Commit

---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Berkay Tuncel on 20.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
