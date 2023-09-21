38de284bbd4a236766c63f74f078b22570fa960a
Berkay Tuncel <berkaytncl98@gmail.com>
2023-09-21 12:34:37 +0300

    Edit multiline text with UITextView

--- Project19/Extension/Action.js
+++ Project19/Extension/Action.js
@@ -3,11 +3,12 @@ var Action = function() { }
 Action.prototype = {
     
 run: function(paramaters) {
-    paramaters.completionFunction({"URL": document.URL, "title": document.title})
+    paramaters.completionFunction({"URL": document.URL, "title": document.title});
 },
     
 finalize: function(paramaters) {
-    
+    var customJavaScript = paramaters["customJavaScript"];
+    eval(customJavaScript);
 }
     
 };
